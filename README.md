dd1458710dc2a38430ce9b92884f7a83e9dd8fd1
isaias1 <isaias_silva189@hotmail.com>
2022-11-11 19:57:05 -0300

    mudanças migration 11/11

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+
+## Code of Conduct[Explicação sobre o que o sistema faz e como usar.docx](https://github.com/Isaiaz1/unasp2022Grupo6/files/9760150/Explicacao.sobre.o.que.o.sistema.faz.e.como.usar.docx)
+
+
+
+![Diagrama Classe](https://user-images.githubusercontent.com/59181631/195211700-7ad8782a-d457-4fd6-9c1b-60089d77bd05.png)
+![Diagrama arquitetura](https://user-images.githubusercontent.com/59181631/195211703-4afee853-4eff-4f90-9a6b-4d27d21e3f6c.jpeg)
+
+
---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//rota de cadastro
+Route::post('/cadastrar-produto', function (Request $request) {
+    Produto::create([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'estoque' => $request->estoque
+
+    ]);
+
+    echo "Produto criado com sucesso!!!";
+
+});
+
+//rota de visualizaçao
+Route::get('/ver-produto/{id}', function ($id) {
+    $produto = Produto::find($id);
+    return view('ver', ['produto' => $produto]);
+});
+
+//rota de edição
+Route::get('/editar-produto/{id}', function ($id) {
+    $produto = Produto::find($id);
+    return view('editar', ['produto' => $produto]);
+});
+
+//rota ediçao 
+Route::post('/editar-produto/{id}', function (Request $request, $id) {
+
+    $produto = Produto::find($id);
+    $produto->update([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'estoque' => $request->estoque
+    ]);
+
+    echo "Produto editado com sucesso!!!";
+
+});
+
+Route::get('/excluir-produto/{id}', function (Request $request, $id) {
+
+    $produto = Produto::find($id);
+    $produto->delete();
+
+    echo "Produto excluido com sucesso!!!";
+
+});
